0deb0515082612f69998cbbc5aaec8e6f58884a6
Apricosma <113317992+Apricosma@users.noreply.github.com>
2026-01-24 16:36:11 -0600

    Prepare container and docker compose

--- AniBento.Api/Program.cs
+++ AniBento.Api/Program.cs
@@ -147,7 +147,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors(allowedOrigins);
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
