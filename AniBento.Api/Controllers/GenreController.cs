b6362f5c650c929c026fd06be7f07543ef0eff87
Apricosma <113317992+Apricosma@users.noreply.github.com>
2026-02-09 12:04:22 -0600

    Create Genre Controller with basic get

---
+++ AniBento.Api/Controllers/GenreController.cs
@@ -0,0 +1,16 @@
+﻿using AniBento.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AniBento.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController(IGenreService genreService) : ControllerBase
+    {
+        public async Task<ActionResult<IEnumerable<string>>> GetGenres(CancellationToken ct)
+        {
+            var genres = await genreService.GetAllAsync(ct);
+            return Ok(genres);
+        }
+    }
+}
