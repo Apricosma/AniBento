24df158951d4974b8fa728fcc2e9a4f24f4d6e03
Apricosma <113317992+Apricosma@users.noreply.github.com>
2026-02-19 14:37:21 -0600

    Change UpdateCollectionRequest to have the same annotations as CreateCollectionRequest

--- AniBento.Api/Dtos/Collection/UpdateCollectionRequest.cs
+++ AniBento.Api/Dtos/Collection/UpdateCollectionRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace AniBento.Api.Dtos.Collection
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AniBento.Api.Dtos.Collection
 {
     public class UpdateCollectionRequest
     {
+        [MaxLength(100)]
         public required string Name { get; set; }
+
+        [MaxLength(1000)]
         public required string Description { get; set; }
         public required bool IsPrivate { get; set; }
     }
